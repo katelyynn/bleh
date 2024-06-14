eb5c2f4c773f5e45ada02b572ff2d11afbbd777e
kate <46572320+katelyynn@users.noreply.github.com>
2024-06-14 03:43:56 +0100

    chore: bump

--- fm/bleh.user.js
+++ fm/bleh.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         bleh
 // @namespace    http://last.fm/
-// @version      2024.0613
+// @version      2024.0614
 // @description  bleh!!! ^-^
 // @author       kate
 // @match        https://www.last.fm/*
@@ -15,7 +15,7 @@
 // @require      https://unpkg.com/tippy.js@6
 // ==/UserScript==
 
-let version = '2024.0613';
+let version = '2024.0614';
 
 tippy.setDefaultProps({
     arrow: false,
