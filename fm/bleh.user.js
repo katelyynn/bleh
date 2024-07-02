8a47c228a45d7b035972980eb7e7699082239aac
kate <46572320+katelyynn@users.noreply.github.com>
2024-07-03 00:38:39 +0100

    fix: bio being empty

--- fm/bleh.user.js
+++ fm/bleh.user.js
@@ -5631,35 +5631,42 @@ let bleh_regex = new RegExp('^https://www\.last\.fm/[a-z]+/bleh$');
             let stat_loved_tracks = (header_metadata[2] != undefined) ? header_metadata[2].querySelector('a') : placeholder_loved_tracks();
 
 
-            let profile_bio = col_sidebar.querySelector('.about-me-sidebar p').textContent;
-            let converter = new showdown.Converter({
-                emoji: true,
-                excludeTrailingPunctuationFromURLs: true,
-                ghMentions: true,
-                ghMentionsLink: '/user/{u}',
-                headerLevelStart: 5,
-                noHeaderId: true,
-                openLinksInNewWindow: true,
-                requireSpaceBeforeHeadingText: true,
-                simpleLineBreaks: true,
-                simplifiedAutoLink: true,
-                strikethrough: true,
-                underline: true,
-                ghCodeBlocks: false,
-                smartIndentationFix: true
-            });
-            let parsed_profile_bio = converter.makeHtml(profile_bio
-            .replace(/([@])([a-zA-Z0-9_]+)/g, '[$1$2](/user/$2)')
-            .replace(/\[artist\]([a-zA-Z0-9]+)\[\/artist\]/g, '[$1](/music/$1)')
-            .replace(/\[album artist=([a-zA-Z0-9]+)\]([a-zA-Z0-9\s]+)\[\/album\]/g, '[$2](/music/$1/$2)')
-            .replace(/\[track artist=([a-zA-Z0-9]+)\]([a-zA-Z0-9\s]+)\[\/track\]/g, '[$2](/music/$1/_/$2)')
-            .replace(/https:\/\/open\.spotify\.com\/user\/([A-Za-z0-9]+)/g, '[@$1](https://open.spotify.com/user/$1)')
-            .replace(/https:\/\/open\.spotify\.com\/user\/([A-Za-z0-9]+)\?si=([A-Za-z0-9]+)/g, '[@$1](https://open.spotify.com/user/$1)')
-            .replace(/&/g, '&amp;')
-            .replace(/</g, '&lt;')
-            .replace(/>/g, '&gt;')
-            .replace(/"/g, '&quot;')
-            .replace(/'/g, '&#039;'));
+            let profile_bio_raw = col_sidebar.querySelector('.about-me-sidebar p');
+            let profile_bio_parsed = 'very mysterious . . .';
+            let profile_is_empty = true;
+            if (profile_bio_raw != null) {
+                profile_is_empty = false;
+
+                let converter = new showdown.Converter({
+                    emoji: true,
+                    excludeTrailingPunctuationFromURLs: true,
+                    ghMentions: true,
+                    ghMentionsLink: '/user/{u}',
+                    headerLevelStart: 5,
+                    noHeaderId: true,
+                    openLinksInNewWindow: true,
+                    requireSpaceBeforeHeadingText: true,
+                    simpleLineBreaks: true,
+                    simplifiedAutoLink: true,
+                    strikethrough: true,
+                    underline: true,
+                    ghCodeBlocks: false,
+                    smartIndentationFix: true
+                });
+                profile_bio_parsed = converter.makeHtml(profile_bio_raw.textContent
+                .replace(/([@])([a-zA-Z0-9_]+)/g, '[$1$2](/user/$2)')
+                .replace(/\[artist\]([a-zA-Z0-9]+)\[\/artist\]/g, '[$1](/music/$1)')
+                .replace(/\[album artist=([a-zA-Z0-9]+)\]([a-zA-Z0-9\s]+)\[\/album\]/g, '[$2](/music/$1/$2)')
+                .replace(/\[track artist=([a-zA-Z0-9]+)\]([a-zA-Z0-9\s]+)\[\/track\]/g, '[$2](/music/$1/_/$2)')
+                .replace(/https:\/\/open\.spotify\.com\/user\/([A-Za-z0-9]+)/g, '[@$1](https://open.spotify.com/user/$1)')
+                .replace(/https:\/\/open\.spotify\.com\/user\/([A-Za-z0-9]+)\?si=([A-Za-z0-9]+)/g, '[@$1](https://open.spotify.com/user/$1)')
+                .replace(/&/g, '&amp;')
+                .replace(/</g, '&lt;')
+                .replace(/>/g, '&gt;')
+                .replace(/"/g, '&quot;')
+                .replace(/'/g, '&#039;'));
+            }
+
 
 
             let follow_button_wrap = profile_header.querySelector('[data-toggle-button=""]');
@@ -5684,8 +5691,8 @@ let bleh_regex = new RegExp('^https://www\.last\.fm/[a-z]+/bleh$');
                     </span>
                 </div>
                 <div class="bottom-wiki">
-                    <div class="profile-bio">
-                        ${parsed_profile_bio}
+                    <div class="profile-bio ${(profile_is_empty) ? 'profile-bio-empty' : ''}">
+                        ${profile_bio_parsed}
                     </div>
                     <div class="actions">
                         ${(msg_button != null) ? msg_button.outerHTML : ''}
