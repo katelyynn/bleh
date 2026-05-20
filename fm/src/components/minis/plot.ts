893f3e340cb1d5bdc097272d5ed4a18f23577152
katelyn <46572320+katelyynn@users.noreply.github.com>
2026-05-20 20:10:04 +0100

    fix: lighterhtml

--- fm/src/components/minis/plot.ts
+++ fm/src/components/minis/plot.ts
@@ -7,19 +7,24 @@ import { input } from "@/components/settings/input";
 import { icon, icons } from "../shared/icon";
 import { select } from "../settings/select";
 import { hybrid_timeframe_picker } from "../date/timeframe";
-import { sanitise } from "@/build/tools";
+import { romanise, sanitise } from "@/build/tools";
 import JSON5 from 'json5';
 import { log } from "@/build/log";
 import { prep_chart_colours } from "../music/chart";
 import { Chart } from "chart.js";
+import { correct_artist, correct_item_by_artist } from "../music/lotus";
 
 export function plot({ host, sidebar } = {}) {
     if (!host || !sidebar) return;
 
     let data_points = [];
 
+    let temporary_data_source = {};
+
     let current_year = new Date().getFullYear();
     let previous_year = current_year - 1;
+
+    let selected_data_source = '';
 
     let body;
     let timeframe;
@@ -34,37 +39,13 @@ export function plot({ host, sidebar } = {}) {
 
     let allow_adding = true;
 
+    let plot_header_options;
+
     render(host, html`
         <div class="plot-header">
             <div class="plot-header-side plot-header-side-main">
                 <label class="plot-header-label">Add to graph</label>
-                <div class="plot-header-options">
-                    ${data_source = select({
-                        values: [
-                            {
-                                text: 'Data source'
-                            },
-                            {
-                                value: '{artist:"glaive",album:"God Save The Three"}',
-                                text: 'glaive - God Save The Three'
-                            }
-                        ]
-                    })}
-                    ${user = select({
-                        values: [
-                            {
-                                text: 'User'
-                            },
-                            {
-                                value: auth.name,
-                                text: auth.name
-                            }
-                        ]
-                    })}
-                    <button class="btn primary icon" data-type="plus" onclick=${() => add_data_point()} ref=${el => add_data_point_btn = el}>
-                        ${tl(trans.add)}
-                    </button>
-                </div>
+                <div class="plot-header-options" ref=${el => plot_header_options = el} />
             </div>
             <div class="plot-header-side">
                 <label class="plot-header-label">Graph options</label>
@@ -87,13 +68,107 @@ export function plot({ host, sidebar } = {}) {
         </div>
     `);
 
+    update_plot_options();
+
+    function update_plot_options() {
+        const data_source_history = JSON5.parse(localStorage.getItem('bleh_plot_data_history') || '[]');
+
+        const unique_users = [...new Set(data_points.map(item => item.user))];
+
+        const seen_media = new Set();
+        const unique_media = [];
+
+        data_points.forEach(point => {
+            const media_string = JSON5.stringify(point.media);
+
+            if (!seen_media.has(media_string)) {
+                seen_media.add(media_string);
+                unique_media.push(point.media);
+            }
+        });
+
+        const media = [];
+        unique_media.forEach(item => {
+            media.push({
+                value: JSON5.stringify(item),
+                text: plot_media_title(item, true)
+            });
+        });
+
+        const media_history = [];
+        data_source_history.forEach(point => {
+            const media_string = JSON5.stringify(point);
+
+            if (!unique_media.includes(media_string)) {
+                media_history.push({
+                    value: media_string,
+                    text: plot_media_title(point, true)
+                })
+            }
+        });
+
+        if (Object.keys(temporary_data_source).length > 0) {
+            media.unshift({
+                value: JSON5.stringify(temporary_data_source),
+                text: plot_media_title(temporary_data_source, true)
+            });
+        }
+
+        console.info('media', media, media_history);
+
+        render(plot_header_options, html`
+            ${data_source = select({
+                values: [
+                    {
+                        text: 'Data source'
+                    },
+                    {
+                        type: 'plus',
+                        text: tl(trans.add),
+                        action: add_new_data_source
+                    },
+                    {
+                        text: 'sep'
+                    },
+                    ...media,
+                    {
+                        text: 'sep'
+                    },
+                    ...media_history
+                ],
+                func: (val: string) => {
+                    selected_data_source = val;
+                },
+                initial: selected_data_source
+            })}
+            ${user = select({
+                values: [
+                    {
+                        text: 'User'
+                    },
+                    {
+                        value: auth.name,
+                        text: auth.name
+                    },
+                    {
+                        value: 'evangelicgirl',
+                        text: 'evangelicgirl'
+                    }
+                ]
+            })}
+            <button class="btn primary icon" data-type="plus" onclick=${() => add_data_point()} ref=${el => add_data_point_btn = el}>
+                ${tl(trans.add)}
+            </button>
+        `);
+    }
+
     prep_chart_colours();
 
     let scrobble_canvas_container = document.createElement('div');
-    scrobble_canvas_container.classList.add('scrobble-canvas-container', 'icon-mask');
+    scrobble_canvas_container.classList.add('plot-canvas-container');
 
     let scrobble_canvas = document.createElement('canvas');
-    scrobble_canvas.classList.add('scrobble-canvas');
+    scrobble_canvas.classList.add('plot-canvas');
 
     let gradient = scrobble_canvas
         .getContext('2d')
@@ -134,7 +209,7 @@ export function plot({ host, sidebar } = {}) {
                         },
                         afterTitle: (context) => {
                             const point = context[0].dataset;
-                            return JSON.stringify(point.media);
+                            return plot_media_title(point.media);
                         },
                         label: (context) => {
                             const point = context.raw;
@@ -260,4 +335,73 @@ export function plot({ host, sidebar } = {}) {
 
         add_data_point_btn.disabled = false;
     }
+
+    function add_new_data_source() {
+        let artist;
+        let album;
+        let track;
+
+        dialog({
+            id: 'add_new_data_source',
+            title: 'Add new data source',
+            body: html.node`
+                ${artist = input({
+                    warn_if_empty: true
+                })}
+                ${album = input({
+                    placeholder: 'album'
+                })}
+                ${track = input({
+                    placeholder: 'track'
+                })}
+                <button class="btn primary icon" data-type="plus" onclick=${complete_add}>
+                    ${tl(trans.add)}
+                </button>
+            `
+        });
+
+        function complete_add() {
+            temporary_data_source = {
+                artist: artist.value
+            }
+
+            if (album.value) {
+                temporary_data_source.album = album.value;
+            } else if (track.value) {
+                temporary_data_source.track = track.value;
+            }
+
+            dialog_rm({ id: 'add_new_data_source' });
+            selected_data_source = JSON5.stringify(temporary_data_source);
+            update_plot_options();
+        }
+    }
+}
+
+interface plot_media {
+    artist: string,
+    album?: string,
+    track?: string
+}
+
+function plot_media_title(data: plot_media, fancy = false) {
+    let text;
+    let icon_name;
+
+    if (data.album) {
+        text = `${romanise(correct_artist(data.artist))} - ${romanise(correct_item_by_artist(data.album, data.artist))}`;
+        icon_name = icons.album;
+    } else if (data.track) {
+        text = `${romanise(correct_artist(data.artist))} - ${romanise(correct_item_by_artist(data.track, data.artist))}`;
+        icon_name = icons.track;
+    } else {
+        text = romanise(correct_artist(data.artist));
+        icon_name = icons.artist;
+    }
+
+    if (fancy) {
+        return html`<span class="bleh-icon" data-type=${icon_name} style="--icon: var(--mask)" />${text}`;
+    }
+
+    return text;
 }
